1e92abd1faf9277685c4a06ec013349afb059513
vivekshubhu <bibek.karki001@gmail.com>
2022-11-06 17:11:35 -0600

    adds assignment 5 part 1

--- app/src/main/java/com/miu/mdp/MainActivity.kt
+++ app/src/main/java/com/miu/mdp/MainActivity.kt
@@ -1,40 +1,116 @@
 package com.miu.mdp
 
 import android.os.Bundle
+import android.widget.CompoundButton
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import android.widget.CompoundButton.OnCheckedChangeListener
 
-class MainActivity : AppCompatActivity() {
+import java.util.*
 
+class MainActivity : AppCompatActivity(), OnCheckedChangeListener {
+
+
+
+
+    private var answer1: String = "compileSdkVersion"
+    private var answer2 = mutableListOf("INVISIBLE", "GONE")
+
+    private var userAnswer1: String = ""
+    private var userAnswer2 = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        forgotPasswordTextView.setOnClickListener {
-            Toast.makeText(this, "Forgot Password Clicked", Toast.LENGTH_SHORT).show()
+//        minSdkVersion
+//
+//        compileSdkVersion
+//
+//        targetSdkVersion
+//
+//        testSdkVersion
+
+        // Question 1
+        radioGroup1.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.radioButton1 -> userAnswer1 = radioButton1.text.toString()
+                R.id.radioButton2 -> userAnswer1 = radioButton2.text.toString()
+                R.id.radioButton3 -> userAnswer1 = radioButton3.text.toString()
+                R.id.radioButton4 -> userAnswer1 = radioButton4.text.toString()
+            }
         }
 
-        signInButton.setOnClickListener {
-            if (emailEditText.text.toString().isEmpty()) {
-                emailEditText.error = "Email is required"
+        // Question 2
+        checkBox1.setOnCheckedChangeListener(this)
+        checkBox2.setOnCheckedChangeListener(this)
+        checkBox3.setOnCheckedChangeListener(this)
+        checkBox4.setOnCheckedChangeListener(this)
+        checkBox5.setOnCheckedChangeListener(this)
+
+        submitBtn.setOnClickListener {
+
+            if (userAnswer1.isEmpty() || userAnswer2.isEmpty()) {
+                Toast.makeText(this, "Please answer all questions", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            if (passwordEditText.text.toString().isEmpty()) {
-                passwordEditText.error = "Password is required"
-                return@setOnClickListener
+            var score = 0
+            if (userAnswer1 == answer1) {
+                score += 50
             }
+            if (userAnswer2.containsAll(answer2)) {
+                score += 50
+            }
+            // current date and time in format
+            val date = Calendar.getInstance().time
+            val formatter = SimpleDateFormat("MM/dd/yyyy, hh:mm:ss a", Locale.getDefault())
+            val currentDateTime = formatter.format(date)
 
-            Toast.makeText(
-                this,
-                "Sign In: email: ${emailEditText.text} password: ${passwordEditText.text}",
-                Toast.LENGTH_SHORT
-            ).show()
+
+            val message =
+                if (score > 0) "Congratulations!!! You submitted on $currentDateTime. You achieved $score%"
+                else "Sorry, You submitted on $currentDateTime. You achieved $score%"
+            AlertDialog.Builder(this)
+                .setTitle("Result")
+                .setMessage(message)
+                .setPositiveButton("OK") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+
+
         }
 
-        createAccountButton.setOnClickListener {
-            Toast.makeText(this, "Create Account Clicked", Toast.LENGTH_SHORT).show()
+
+        resetBtn.setOnClickListener {
+            radioGroup1.clearCheck()
+            resetCheckbox()
         }
+
+    }
+
+    private fun resetCheckbox() {
+        checkBox1.isChecked = false
+        checkBox2.isChecked = false
+        checkBox3.isChecked = false
+        checkBox4.isChecked = false
+        checkBox5.isChecked = false
+    }
+
+    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        when (buttonView?.id) {
+            R.id.checkBox1 -> addAnswer("INVISIBLE", isChecked)
+            R.id.checkBox2 -> addAnswer("GONE", isChecked)
+            R.id.checkBox3 -> addAnswer("VISIBLE", isChecked)
+            R.id.checkBox4 -> addAnswer("INVISIBLE", isChecked)
+            R.id.checkBox5 -> addAnswer("GONE", isChecked)
+        }
+    }
+
+    private fun addAnswer(answer: String, isChecked: Boolean) {
+        if (isChecked) userAnswer2.add(answer) else userAnswer2.remove(answer)
     }
 }
